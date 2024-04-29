1d6cbc16c2bcf0ece8c921153123aa1d4f40b635
Carlos V <carlos.vdr@semiotic.ai>
2024-04-29 19:32:24 +0000

    test: Added test for timestamp check

--- common/src/tap/checks/timestamp_check.rs
+++ common/src/tap/checks/timestamp_check.rs
@@ -1,11 +1,10 @@
 // Copyright 2023-, GraphOps and Semiotic Labs.
 // SPDX-License-Identifier: Apache-2.0
-
 use anyhow::anyhow;
 use std::time::{Duration, SystemTime};
 
 pub struct TimestampCheck {
-    timestamp_threshold: Duration,
+    timestamp_error_tolerance: Duration,
 }
 
 use tap_core::receipt::{
@@ -14,9 +13,9 @@ use tap_core::receipt::{
 };
 
 impl TimestampCheck {
-    pub fn new(timestamp_threshold: Duration) -> Self {
+    pub fn new(timestamp_error_tolerance: Duration) -> Self {
         Self {
-            timestamp_threshold,
+            timestamp_error_tolerance,
         }
     }
 }
@@ -25,8 +24,8 @@ impl TimestampCheck {
 impl Check for TimestampCheck {
     async fn check(&self, receipt: &ReceiptWithState<Checking>) -> CheckResult {
         let timestamp_now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?;
-        let min_timestamp = timestamp_now - self.timestamp_threshold;
-        let max_timestamp = timestamp_now + self.timestamp_threshold;
+        let min_timestamp = timestamp_now - self.timestamp_error_tolerance;
+        let max_timestamp = timestamp_now + self.timestamp_error_tolerance;
 
         let receipt_timestamp = Duration::from_nanos(receipt.signed_receipt().message.timestamp_ns);
 
@@ -34,9 +33,75 @@ impl Check for TimestampCheck {
             Ok(())
         } else {
             Err(anyhow!(
-                "Receipt timestamp `{}` is outside threshold",
+                "Receipt timestamp `{}` is outside of current system time +/- timestamp_error_tolerance",
                 receipt_timestamp.as_secs()
             ))
         }
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloy_primitives::Address;
+    use std::str::FromStr;
+
+    use tap_core::{
+        receipt::{checks::Check, Checking, Receipt, ReceiptWithState},
+        signed_message::EIP712SignedMessage,
+    };
+
+    use alloy_sol_types::eip712_domain;
+    use alloy_sol_types::Eip712Domain;
+    use ethers::signers::coins_bip39::English;
+    use ethers::signers::{LocalWallet, MnemonicBuilder};
+    fn create_signed_receipt_with_jitter(jitter: u64) -> ReceiptWithState<Checking> {
+        let index: u32 = 0;
+        let wallet: LocalWallet = MnemonicBuilder::<English>::default()
+            .phrase("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about")
+            .index(index)
+            .unwrap()
+            .build()
+            .unwrap();
+        let eip712_domain_separator: Eip712Domain = eip712_domain! {
+            name: "TAP",
+            version: "1",
+            chain_id: 1,
+            verifying_contract: Address:: from([0x11u8; 20]),
+        };
+        let value: u128 = 1234;
+        let nonce: u64 = 10;
+        let timestamp = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_nanos()
+            + Duration::from_secs(jitter).as_nanos();
+        let timestamp_ns = timestamp as u64;
+        let receipt = EIP712SignedMessage::new(
+            &eip712_domain_separator,
+            Receipt {
+                allocation_id: Address::from_str("0xabababababababababababababababababababab")
+                    .unwrap(),
+                nonce,
+                timestamp_ns,
+                value,
+            },
+            &wallet,
+        )
+        .unwrap();
+        ReceiptWithState::<Checking>::new(receipt)
+    }
+
+    #[tokio::test]
+    async fn test_timestamp_inside_tolerance() {
+        let signed_receipt = create_signed_receipt_with_jitter(15);
+        let timestamp_check = TimestampCheck::new(Duration::from_secs(30));
+        assert!(timestamp_check.check(&signed_receipt).await.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_timestamp_outside_tolerance() {
+        let signed_receipt = create_signed_receipt_with_jitter(31);
+        let timestamp_check = TimestampCheck::new(Duration::from_secs(30));
+        assert!(timestamp_check.check(&signed_receipt).await.is_err());
     }
 }
