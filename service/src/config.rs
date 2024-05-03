93c7427f0d0892a6f7a432a829aa680da68347c3
Alexis Asseman <alexis@semiotic.ai>
2024-05-03 16:57:26 -0700

    docs(service): add min/max config file examples + tests
    The tests ensure that the config files are up to date with the code.
    I also looks like freezing `serde_spanned` is not needed anymore + needed to remove it to be able to add `toml`.
    
    Signed-off-by: Alexis Asseman <alexis@semiotic.ai>

--- service/src/config.rs
+++ service/src/config.rs
@@ -8,9 +8,9 @@ use figment::{
     Figment,
 };
 use indexer_common::indexer_service::http::IndexerServiceConfig;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Config {
     pub common: IndexerServiceConfig,
 }
@@ -18,5 +18,37 @@ pub struct Config {
 impl Config {
     pub fn load(filename: &PathBuf) -> Result<Self, figment::Error> {
         Figment::new().merge(Toml::file(filename)).extract()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+
+    use super::*;
+
+    /// Test loading the minimal configuration example file.
+    /// Makes sure that the minimal template is up to date with the code.
+    #[test]
+    fn test_minimal_config() {
+        Config::load(&PathBuf::from("service/minimal-config-example.toml")).unwrap();
+    }
+
+    /// Test that the maximal configuration file is up to date with the code.
+    /// Make sure that `test_minimal_config` passes before looking at this.
+    #[test]
+    fn test_maximal_config() {
+        // Generate full config by deserializing the minimal config and let the code fill in the defaults.
+        let max_config =
+            Config::load(&PathBuf::from("service/minimal-config-example.toml")).unwrap();
+        // Deserialize the full config example file
+        let max_config_file: toml::Value = toml::from_str(
+            fs::read_to_string("maximal-config-example.toml")
+                .unwrap()
+                .as_str(),
+        )
+        .unwrap();
+
+        assert_eq!(toml::Value::try_from(max_config).unwrap(), max_config_file);
     }
 }
