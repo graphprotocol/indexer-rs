76d7b06624515b352eaf3bc19d5c906df75519e6
Gustavo Inacio <gustavo@semiotic.ai>
2024-08-09 14:41:28 -0300

    feat(common): use channel to batch insert
    Signed-off-by: Gustavo Inacio <gustavo@semiotic.ai>

--- common/src/tap/receipt_store.rs
+++ common/src/tap/receipt_store.rs
@@ -1,17 +1,99 @@
 // Copyright 2023-, GraphOps and Semiotic Labs.
 // SPDX-License-Identifier: Apache-2.0
 
-use alloy::hex::ToHexExt;
+use alloy::{dyn_abi::Eip712Domain, hex::ToHexExt};
 use anyhow::anyhow;
 use bigdecimal::num_bigint::BigInt;
-use sqlx::types::BigDecimal;
+use sqlx::{types::BigDecimal, PgPool};
 use tap_core::{
     manager::adapters::ReceiptStore,
     receipt::{state::Checking, ReceiptWithState},
 };
+use tokio::{select, sync::mpsc::Receiver, task::JoinHandle};
+use tokio_util::sync::CancellationToken;
 use tracing::error;
 
 use super::{AdapterError, IndexerTapContext};
+
+#[derive(Clone)]
+pub struct InnerContext {
+    pub pgpool: PgPool,
+}
+
+impl InnerContext {
+    async fn store_receipts(&self, receipts: Vec<DatabaseReceipt>) -> Result<(), AdapterError> {
+        let receipts_len = receipts.len();
+        let mut signers = Vec::with_capacity(receipts_len);
+        let mut signatures = Vec::with_capacity(receipts_len);
+        let mut allocation_ids = Vec::with_capacity(receipts_len);
+        let mut timestamps = Vec::with_capacity(receipts_len);
+        let mut nonces = Vec::with_capacity(receipts_len);
+        let mut values = Vec::with_capacity(receipts_len);
+
+        for receipt in receipts {
+            signers.push(receipt.signer_address);
+            signatures.push(receipt.signature);
+            allocation_ids.push(receipt.allocation_id);
+            timestamps.push(receipt.timestamp_ns);
+            nonces.push(receipt.nonce);
+            values.push(receipt.value);
+        }
+        sqlx::query!(
+            r#"INSERT INTO scalar_tap_receipts (
+                signer_address,
+                signature,
+                allocation_id,
+                timestamp_ns,
+                nonce,
+                value
+            ) SELECT * FROM UNNEST(
+                $1::CHAR(40)[],
+                $2::BYTEA[],
+                $3::CHAR(40)[],
+                $4::NUMERIC(20)[],
+                $5::NUMERIC(20)[],
+                $6::NUMERIC(40)[]
+            )"#,
+            &signers,
+            &signatures,
+            &allocation_ids,
+            &timestamps,
+            &nonces,
+            &values,
+        )
+        .execute(&self.pgpool)
+        .await
+        .map_err(|e| {
+            error!("Failed to store receipt: {}", e);
+            anyhow!(e)
+        })?;
+
+        Ok(())
+    }
+}
+
+impl IndexerTapContext {
+    pub fn spawn_store_receipt_task(
+        inner_context: InnerContext,
+        mut receiver: Receiver<DatabaseReceipt>,
+        cancelation_token: CancellationToken,
+    ) -> JoinHandle<()> {
+        const BUFFER_SIZE: usize = 100;
+        tokio::spawn(async move {
+            loop {
+                let mut buffer = Vec::with_capacity(BUFFER_SIZE);
+                select! {
+                    _ = cancelation_token.cancelled() => { break },
+                    _ = receiver.recv_many(&mut buffer, BUFFER_SIZE) => {
+                        if let Err(e) = inner_context.store_receipts(buffer).await {
+                            error!("Failed to store receipts: {}", e);
+                        }
+                    }
+                }
+            }
+        })
+    }
+}
 
 #[async_trait::async_trait]
 impl ReceiptStore for IndexerTapContext {
@@ -21,38 +103,53 @@ impl ReceiptStore for IndexerTapContext {
         &self,
         receipt: ReceiptWithState<Checking>,
     ) -> Result<u64, Self::AdapterError> {
-        let receipt = receipt.signed_receipt();
-        let allocation_id = receipt.message.allocation_id;
-        let encoded_signature = receipt.signature.as_bytes().to_vec();
-
-        let receipt_signer = receipt
-            .recover_signer(self.domain_separator.as_ref())
-            .map_err(|e| {
-                error!("Failed to recover receipt signer: {}", e);
-                anyhow!(e)
-            })?;
-
-        // TODO: consider doing this in another async task to avoid slowing down the paid query flow.
-        sqlx::query!(
-            r#"
-                INSERT INTO scalar_tap_receipts (signer_address, signature, allocation_id, timestamp_ns, nonce, value)
-                VALUES ($1, $2, $3, $4, $5, $6)
-            "#,
-            receipt_signer.encode_hex(),
-            encoded_signature,
-            allocation_id.encode_hex(),
-            BigDecimal::from(receipt.message.timestamp_ns),
-            BigDecimal::from(receipt.message.nonce),
-            BigDecimal::from(BigInt::from(receipt.message.value)),
-        )
-        .execute(&self.pgpool)
-        .await
-        .map_err(|e| {
+        let db_receipt = DatabaseReceipt::from_receipt(receipt, &self.domain_separator)?;
+        self.receipt_producer.send(db_receipt).await.map_err(|e| {
             error!("Failed to store receipt: {}", e);
             anyhow!(e)
         })?;
 
         // We don't need receipt_ids
         Ok(0)
+    }
+}
+
+pub struct DatabaseReceipt {
+    signer_address: String,
+    signature: Vec<u8>,
+    allocation_id: String,
+    timestamp_ns: BigDecimal,
+    nonce: BigDecimal,
+    value: BigDecimal,
+}
+
+impl DatabaseReceipt {
+    fn from_receipt(
+        receipt: ReceiptWithState<Checking>,
+        separator: &Eip712Domain,
+    ) -> anyhow::Result<Self> {
+        let receipt = receipt.signed_receipt();
+        let allocation_id = receipt.message.allocation_id.encode_hex();
+        let signature = receipt.signature.as_bytes().to_vec();
+
+        let signer_address = receipt
+            .recover_signer(separator)
+            .map_err(|e| {
+                error!("Failed to recover receipt signer: {}", e);
+                anyhow!(e)
+            })?
+            .encode_hex();
+
+        let timestamp_ns = BigDecimal::from(receipt.message.timestamp_ns);
+        let nonce = BigDecimal::from(receipt.message.nonce);
+        let value = BigDecimal::from(BigInt::from(receipt.message.value));
+        Ok(Self {
+            allocation_id,
+            nonce,
+            signature,
+            signer_address,
+            timestamp_ns,
+            value,
+        })
     }
 }
