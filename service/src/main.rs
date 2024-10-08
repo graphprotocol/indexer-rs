1b89ec836c3447f36d554a9895997fb5e66b69b6
Carlos V <carlos.vdr@semiotic.ai>
2024-10-08 16:44:27 +0000

    feat: Add tracing subscriber to indexer-service

--- service/src/main.rs
+++ service/src/main.rs
@@ -4,10 +4,26 @@
 use std::process::ExitCode;
 
 use indexer_service_rs::service::run;
+use tracing::level_filters::LevelFilter;
+use tracing::subscriber::set_global_default;
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
 
 #[tokio::main]
 async fn main() -> ExitCode {
-    tracing_subscriber::fmt::init();
+    // Tracing setup
+    let filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env_lossy();
+    let subscriber_builder: tracing_subscriber::fmt::SubscriberBuilder<
+        tracing_subscriber::fmt::format::DefaultFields,
+        tracing_subscriber::fmt::format::Format,
+        EnvFilter,
+    > = FmtSubscriber::builder().with_env_filter(filter);
+    set_global_default(subscriber_builder.with_ansi(true).pretty().finish()).expect(
+        "Could not set up global default subscriber for logger, check \
+        environmental variable `RUST_LOG`",
+    );
+
     if let Err(e) = run().await {
         tracing::error!("Indexer service error: {e}");
         return ExitCode::from(1);
