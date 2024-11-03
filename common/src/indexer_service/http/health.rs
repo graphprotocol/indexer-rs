ff2a9f087a161ea6a40a0e0aa01be19432b7f301
shiyasmohd <shiyasjaseena124@gmail.com>
2024-11-03 12:54:50 +0530

    feat: add error message if subgraph is unhealthy/failed

--- common/src/indexer_service/http/health.rs
+++ common/src/indexer_service/http/health.rs
@@ -9,7 +9,7 @@ use axum::{
 };
 use indexer_config::GraphNodeConfig;
 use reqwest::StatusCode;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 use thiserror::Error;
 
@@ -25,8 +25,16 @@ struct SubgraphData {
 }
 
 #[derive(Deserialize, Debug)]
+#[allow(non_snake_case)]
 struct IndexingStatus {
     health: Health,
+    fatalError: Option<Message>,
+    nonFatalErrors: Vec<Message>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Message {
+    message: String,
 }
 
 #[derive(Deserialize, Debug)]
@@ -49,8 +57,6 @@ impl Health {
 
 #[derive(Debug, Error)]
 pub enum CheckHealthError {
-    #[error("Graph node config not found")]
-    GraphNodeConfigNotFound,
     #[error("Deployment not found")]
     DeploymentNotFound,
     #[error("Failed to process query")]
@@ -60,9 +66,6 @@ pub enum CheckHealthError {
 impl IntoResponse for CheckHealthError {
     fn into_response(self) -> AxumResponse {
         let (status, error_message) = match &self {
-            CheckHealthError::GraphNodeConfigNotFound => {
-                (StatusCode::NOT_FOUND, "Graph node config not found")
-            }
             CheckHealthError::DeploymentNotFound => (StatusCode::NOT_FOUND, "Deployment not found"),
             CheckHealthError::QueryForwardingError => {
                 (StatusCode::INTERNAL_SERVER_ERROR, "Failed to process query")
@@ -79,19 +82,19 @@ impl IntoResponse for CheckHealthError {
 
 pub async fn health(
     Path(deployment_id): Path<String>,
-    Extension(graph_node): Extension<Option<GraphNodeConfig>>,
+    Extension(graph_node): Extension<GraphNodeConfig>,
 ) -> Result<impl IntoResponse, CheckHealthError> {
-    let url = if let Some(graph_node) = graph_node {
-        graph_node.status_url
-    } else {
-        return Err(CheckHealthError::GraphNodeConfigNotFound);
-    };
-
     let body = Query::new_with_variables(
         r#"
             query indexingStatuses($ids: [String!]!) {
                 indexingStatuses(subgraphs: $ids) {
                     health
+                    fatalError {
+                        message
+                    }
+                    nonFatalErrors {
+                        message
+                    }
                 }
             }
         "#,
@@ -99,17 +102,31 @@ pub async fn health(
     );
 
     let client = reqwest::Client::new();
-    let response = client.post(url).json(&body).send().await;
-    let response = response.expect("Failed to get response");
-    let response_json: Result<Response, reqwest::Error> = response.json().await;
+    let response = client.post(graph_node.status_url).json(&body).send().await;
+    let res = response.expect("Failed to get response");
+    let response_json: Result<Response, reqwest::Error> = res.json().await;
 
     match response_json {
         Ok(res) => {
             if res.data.indexingStatuses.is_empty() {
                 return Err(CheckHealthError::DeploymentNotFound);
             };
-            let health_status = res.data.indexingStatuses[0].health.as_str();
-            Ok(Json(json!({ "health": health_status })))
+            let status = &res.data.indexingStatuses[0];
+            let health_response = match status.health {
+                Health::healthy => json!({ "health": status.health.as_str() }),
+                Health::unhealthy => {
+                    let errors: Vec<&String> = status
+                        .nonFatalErrors
+                        .iter()
+                        .map(|msg| &msg.message)
+                        .collect();
+                    json!({ "health": status.health.as_str(), "nonFatalErrors": errors })
+                }
+                Health::failed => {
+                    json!({ "health": status.health.as_str(), "fatalError": status.fatalError.as_ref().map_or("null", |msg| &msg.message) })
+                }
+            };
+            Ok(Json(health_response))
         }
         Err(_) => Err(CheckHealthError::QueryForwardingError),
     }
