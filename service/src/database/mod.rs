e0dcc08e900f5607cf38a5ed1f6e025fd71368b2
Filipe Azevedo <filipe@azevedo.io>
2024-11-07 22:27:13 +0000

    chore: Bump alloy

---
+++ service/src/database/mod.rs
@@ -0,0 +1,20 @@
+// Copyright 2023-, Edge & Node, GraphOps, and Semiotic Labs.
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod dips;
+
+use std::time::Duration;
+
+use sqlx::{postgres::PgPoolOptions, PgPool};
+use tracing::debug;
+
+pub async fn connect(url: &str) -> PgPool {
+    debug!("Connecting to database");
+
+    PgPoolOptions::new()
+        .max_connections(50)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(url)
+        .await
+        .expect("Should be able to connect to the database")
+}
