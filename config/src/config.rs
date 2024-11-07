a64ce3efea3ba7c95b54023dd4a554af1224ba96
Filipe Azevedo <filipe@azevedo.io>
2024-11-07 22:27:13 +0000

    feat: Add DIPS support

--- config/src/config.rs
+++ config/src/config.rs
@@ -41,6 +41,7 @@ pub struct Config {
     pub blockchain: BlockchainConfig,
     pub service: ServiceConfig,
     pub tap: TapConfig,
+    pub dips: DipsConfig,
 }
 
 // Newtype wrapping Config to be able use serde_ignored with Figment
@@ -380,6 +381,13 @@ pub struct TapConfig {
     pub rav_request: RavRequestConfig,
 
     pub sender_aggregator_endpoints: HashMap<Address, Url>,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[cfg_attr(test, derive(PartialEq))]
+pub struct DipsConfig {
+    pub allowed_payers: Vec<Address>,
+    pub cancellation_time_tolerance: Option<Duration>,
 }
 
 impl TapConfig {
