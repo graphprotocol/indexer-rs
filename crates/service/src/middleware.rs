6202b0cb3299997d013cbaa66740e1f7d96baf0d
Gustavo Inacio <gustavo@semiotic.ai>
2024-11-20 15:31:22 -0600

    refactor: create inject_deployment middleware
    Signed-off-by: Gustavo Inacio <gustavo@semiotic.ai>

--- crates/service/src/middleware.rs
+++ crates/service/src/middleware.rs
@@ -1,11 +1,4 @@
 // Copyright 2023-, Edge & Node, GraphOps, and Semiotic Labs.
 // SPDX-License-Identifier: Apache-2.0
 
-mod cli;
-mod database;
-mod error;
-mod middleware;
-mod routes;
-pub mod service;
-mod tap;
-mod wallet;
+mod inject_deployment;
