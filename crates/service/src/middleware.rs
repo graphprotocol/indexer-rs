5613baa55f8bd27b7775bcc7974b4b0917882fef
Gustavo Inacio <gustavo@semiotic.ai>
2024-11-21 18:05:51 -0600

    refactor: rename metrics to prometheus metrics
    Signed-off-by: Gustavo Inacio <gustavo@semiotic.ai>

--- crates/service/src/middleware.rs
+++ crates/service/src/middleware.rs
@@ -6,11 +6,11 @@ mod inject_deployment;
 mod inject_labels;
 mod inject_receipt;
 mod inject_sender;
-mod metrics;
+mod prometheus_metrics;
 
 pub use inject_allocation::{allocation_middleware, Allocation, AllocationState};
 pub use inject_deployment::deployment_middleware;
 pub use inject_labels::labels_middleware;
 pub use inject_receipt::receipt_middleware;
 pub use inject_sender::{sender_middleware, Sender, SenderState};
-pub use metrics::MetricsMiddlewareLayer;
+pub use prometheus_metrics::PrometheusMetricsMiddlewareLayer;
