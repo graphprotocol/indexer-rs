3895893775aec4f129fc1c7ff6fc900e86c14f28
Gustavo Inacio <gustavo@semiotic.ai>
2024-11-21 19:34:03 -0600

    refactor: add attestation middleware
    Signed-off-by: Gustavo Inacio <gustavo@semiotic.ai>

--- crates/service/src/middleware.rs
+++ crates/service/src/middleware.rs
@@ -1,6 +1,7 @@
 // Copyright 2023-, Edge & Node, GraphOps, and Semiotic Labs.
 // SPDX-License-Identifier: Apache-2.0
 
+mod attestation;
 pub mod auth;
 mod inject_allocation;
 mod inject_attestation_signer;
