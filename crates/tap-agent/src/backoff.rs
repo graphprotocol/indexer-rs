d40cb19c2130c403378b4c9fa847cfefc28b667f
Gustavo Inacio <gustavo@semiotic.ai>
2025-02-05 21:30:20 +0100

    docs: add backoff docs
    Signed-off-by: Gustavo Inacio <gustavo@semiotic.ai>

--- crates/tap-agent/src/backoff.rs
+++ crates/tap-agent/src/backoff.rs
@@ -1,8 +1,19 @@
 // Copyright 2023-, Edge & Node, GraphOps, and Semiotic Labs.
 // SPDX-License-Identifier: Apache-2.0
 
+//! # backoff
+//! This module is used to provide a helper that keep tracks of exponential backoff information in a
+//! non-blocking way. This is important since Actors process one message at a time, and an sleep in
+//! the middle would affect performance.
+//!
+//! This way we just mark something as "in backoff" and just check that information before sending
+//! the request.
+//!
+//! This module is also used by [crate::tracker].
+
 use std::time::{Duration, Instant};
 
+/// Backoff information based on [Instant]
 #[derive(Debug, Clone)]
 pub struct BackoffInfo {
     failed_count: u32,
@@ -10,10 +21,16 @@ pub struct BackoffInfo {
 }
 
 impl BackoffInfo {
+    /// Callback represinging that a request succeed
+    ///
+    /// This resets the failed_count
     pub fn ok(&mut self) {
         self.failed_count = 0;
     }
 
+    /// Callback represinging that a request failed
+    ///
+    /// This sets the backoff time to max(100ms * 2 ^ retries, 60s)
     pub fn fail(&mut self) {
         // backoff = max(100ms * 2 ^ retries, 60s)
         self.failed_backoff_time = Instant::now()
@@ -22,6 +39,7 @@ impl BackoffInfo {
         self.failed_count += 1;
     }
 
+    /// Returns if is in backoff
     pub fn in_backoff(&self) -> bool {
         let now = Instant::now();
         now < self.failed_backoff_time
