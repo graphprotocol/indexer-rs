3d5bbd6571f460167ed8893c9d8bbbe9322552e3
Gustavo Inacio <gustavo@semiotic.ai>
2025-02-13 19:55:04 +0100

    refactor: update deny list check for v2
    Signed-off-by: Gustavo Inacio <gustavo@semiotic.ai>

--- migrations/20250212211337_tap_horizon_sender_denylist.down.sql
+++ migrations/20250212211337_tap_horizon_sender_denylist.down.sql
@@ -1,2 +1,6 @@
 -- Add down migration script here
+DROP TRIGGER IF EXISTS deny_update ON tap_horizon_deny CASCADE;
+
+DROP FUNCTION IF EXISTS tap_horizon_deny_notify() CASCADE;
+
 DROP TABLE IF EXISTS tap_horizon_denylist CASCADE;
