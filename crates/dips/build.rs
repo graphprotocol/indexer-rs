7678dabcd1f31bd2759ec5325e7ec25344472113
Joseph Livesey <joseph@semiotic.ai>
2025-08-15 18:24:40 -0400

    build: update protobuf crates to 0.14.1
    Signed-off-by: Joseph Livesey <joseph@semiotic.ai>

--- crates/dips/build.rs
+++ crates/dips/build.rs
@@ -6,14 +6,16 @@ fn main() {
     {
         println!("cargo:rerun-if-changed=proto");
 
-        tonic_build::configure()
+        tonic_prost_build::configure()
+            .build_server(true)
             .out_dir("src/proto")
             .include_file("indexer.rs")
             .protoc_arg("--experimental_allow_proto3_optional")
             .compile_protos(&["proto/indexer.proto"], &["proto/"])
             .expect("Failed to compile DIPs indexer RPC proto(s)");
 
-        tonic_build::configure()
+        tonic_prost_build::configure()
+            .build_server(true)
             .out_dir("src/proto")
             .include_file("gateway.rs")
             .protoc_arg("--experimental_allow_proto3_optional")
