c848a0c88da14086d2e613e768d9551b231940c1
Natanael Mojica <neithanmo@gmail.com>
2025-09-26 16:01:25 -0600

    ci: fix formatting issues

--- crates/service/src/tap/checks.rs
+++ crates/service/src/tap/checks.rs
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 pub mod allocation_eligible;
-pub mod deny_list_check;
 pub mod data_service_check;
+pub mod deny_list_check;
 pub mod receipt_max_val_check;
 pub mod sender_balance_check;
 pub mod timestamp_check;
